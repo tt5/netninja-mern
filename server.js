47940a9c87f8237e9fac546a61465a382c8838a1
n <2585803+tt5@users.noreply.github.com>
2022-06-22 19:31:30 +0200

    first commit, lesson 2

---
+++ server.js
@@ -0,0 +1,18 @@
+require('dotenv').config()
+const express = require('express')
+
+const app = express()
+
+app.use((req, res, next) => {
+  console.log(req.path, req.method)
+  next()
+})
+
+app.get('/', (req, res) => {
+  res.json({msg: 'welcome'})
+})
+
+
+app.listen(process.env.PORT, () => {
+  console.log('listening on ', process.env.PORT)
+})
