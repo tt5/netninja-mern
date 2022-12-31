136404dd3e5b4e55585ad83803f87f1ebd46b8b5
n <2585803+tt5@users.noreply.github.com>
2022-12-31 15:10:23 +0100

    #6

--- routes/workouts.js
+++ routes/workouts.js
@@ -1,26 +1,17 @@
 const express = require("express");
-const Workout = require("../models/workoutModel");
+const {
+  getWorkouts, 
+  getWorkout, 
+  createWorkout,
+} = require('../controllers/workoutController');
 
 const router = express.Router();
 
-router.get("/", (req, res) => {
-  res.json({ msg: "GET all workouts" });
-});
+router.get("/", getWorkouts);
 
-router.get("/:id", (req, res) => {
-  res.json({ msg: "GET single workouts" });
-});
+router.get("/:id", getWorkout);
 
-router.post("/", async (req, res) => {
-  const { title, load, reps } = req.body;
-
-  try {
-    const workout = await Workout.create({ title, load, reps });
-    res.status(200).json(workout);
-  } catch (error) {
-    res.status(400).json({ error: error.message });
-  }
-});
+router.post("/", createWorkout);
 
 router.delete("/", (req, res) => {
   res.json({ msg: "DELETE a new workout" });
